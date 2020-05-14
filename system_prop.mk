e9f9c565a448c46079e6e8cdc0318cfcc7741670
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2020-05-14 18:30:30 +0000

    universal8895-common: Enable ADB on boot
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -79,6 +79,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=5 \
     debug.sf.latch_unsignaled=1 \
     debug.stagefright.ccodec=0
+	
+# USB debugging
+persist.sys.usb.config=mtp,adb
+ro.adb.secure=0
+ro.secure=0
+ro.debuggable=1
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
